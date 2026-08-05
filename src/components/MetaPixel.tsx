9e6b30a9cd030f56308b90dbcc6b4dc7ddb47828
Abduvois <abduvoisov@gmail.com>
2026-08-05 09:25:17 +0500

    fix(pixel): ensure Meta Pixel script renders with dangerouslySetInnerHTML

--- src/components/MetaPixel.tsx
+++ src/components/MetaPixel.tsx
@@ -32,22 +32,26 @@ function MetaPixelTracker({ pixelId }: { pixelId: string }) {
 export default function MetaPixel({ pixelId }: { pixelId?: string }) {
   const id = pixelId || META_PIXEL_ID;
 
+  const scriptHtml = `
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '${id}');
+    fbq('track', 'PageView');
+  `;
+
   return (
     <>
-      <Script id="meta-pixel" strategy="afterInteractive">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '${id}');
-          fbq('track', 'PageView');
-        `}
-      </Script>
+      <Script
+        id="meta-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{ __html: scriptHtml }}
+      />
       <noscript>
         <img
           height="1"
